bc297251138eac2b2065e9c51adf09f30e2f336a
Ömer Okumuş <omerokumus3@gmail.com>
2023-05-06 00:46:34 +0300

    StatusBar

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
 import CategoriesScreen from './screens/CategoriesScreen';
 
 export default function App() {
-  return <CategoriesScreen />;
+  return (
+    <>
+      <StatusBar style="light" />
+      <CategoriesScreen />
+    </>
+  );
 }
