b95315cf67a2abe2e3e2c2ea952f98d8a06836b0
Ömer Okumuş <omerokumus3@gmail.com>
2023-05-07 09:34:45 +0300

    screen options for MealsOverviewScreen

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,15 +1,31 @@
 import { StyleSheet, FlatList, View } from 'react-native';
-import { MEALS } from '../data/dummy-data';
+import { MEALS, CATEGORIES } from '../data/dummy-data';
 import MealItem from '../components/MealItem';
+import { useLayoutEffect } from 'react';
 
-function MealsOverviewScreen({ route }) {
+function MealsOverviewScreen({ route, navigation }) {
   const categoryId = route.params.categoryId;
   const displayedMeals = MEALS.filter(
     (m) => m.categoryIds.indexOf(categoryId) >= 0
   );
 
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === categoryId
+    ).title;
+    navigation.setOptions({ title: categoryTitle });
+  }, [categoryId, navigation]);
+
   function renderMealItem(itemData) {
-    return <MealItem title={itemData.item.title} />;
+    const item = itemData.item;
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      duration: item.duration,
+      complexity: item.complexity,
+      affordability: item.affordability,
+    };
+    return <MealItem {...mealItemProps} />;
   }
   return (
     <View styles={styles.container}>
