c01e49823a5d6b48f48806653ee7f5402e53f506
Ömer Okumuş <omerokumus3@gmail.com>
2023-05-08 09:07:29 +0300

    MealDetailScreen styling and page structure

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,11 +1,60 @@
 import { useLayoutEffect } from 'react';
-import { Text } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { MEALS } from '../data/dummy-data';
+import MealDetail from '../components/MealDetail';
+import Subtitle from '../components/MetalDetail/Subtitle';
+import List from '../components/MetalDetail/List';
 
 function MealDetailScreen({ route, navigation }) {
+  const meal = MEALS.find((meal) => meal.id === route.params.id);
+  const { duration, complexity, affordability } = meal;
   useLayoutEffect(() => {
-    navigation.setOptions({ title: route.params.title });
+    navigation.setOptions({ title: meal.title });
   });
-  return <Text>{route.params.title}</Text>;
+  return (
+    <ScrollView style={styles.rootContainer}>
+      <Image style={styles.image} source={{ uri: meal.imageUrl }} />
+      <Text style={styles.title}>{meal.title}</Text>
+      <MealDetail
+        duration={duration}
+        complexity={complexity}
+        affordability={affordability}
+        textStyle={styles.detailText}
+      />
+      <View style={styles.listsOuterContainer}>
+        <View style={styles.listsContainer}>
+          <Subtitle>Ingredients</Subtitle>
+          <List data={meal.ingredients} />
+          <Subtitle>Steps</Subtitle>
+          <List data={meal.steps} />
+        </View>
+      </View>
+    </ScrollView>
+  );
 }
 
 export default MealDetailScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: { marginBottom: 32 },
+  image: {
+    width: '100%',
+    height: 350,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    textAlign: 'center',
+    margin: 8,
+    color: 'white',
+  },
+  detailText: {
+    color: 'white',
+  },
+  listsOuterContainer: {
+    alignItems: 'center',
+  },
+  listsContainer: {
+    width: '80%',
+  },
+});
