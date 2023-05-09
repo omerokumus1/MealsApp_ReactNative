9057c4232cb90414dd700a19d23a6e7c9ed7c369
Ömer Okumuş <omerokumus3@gmail.com>
2023-05-09 13:15:45 +0300

    no favorite meal case

--- screens/FavoriesScreen.js
+++ screens/FavoriesScreen.js
@@ -1,4 +1,4 @@
-import { Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { MEALS } from '../data/dummy-data';
 import MealList from '../components/MealList/MealList';
 import { useContext } from 'react';
@@ -9,7 +9,29 @@ function FavoriesScreen() {
   const favoriteMeals = MEALS.filter((meal) =>
     favoriteMealsCtx.ids.includes(meal.id)
   );
+
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You have no favorite meal yet.</Text>
+      </View>
+    );
+  }
+
   return <MealList items={favoriteMeals} />;
 }
 
 export default FavoriesScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
