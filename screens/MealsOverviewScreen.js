b862ab4314887bcbded26aef35d79ffd74f78705
Ömer Okumuş <omerokumus3@gmail.com>
2023-05-09 13:11:51 +0300

    MealList is applied to MealsOverviewScreen

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,6 +1,6 @@
-import { StyleSheet, FlatList, View } from 'react-native';
+import MealList from '../components/MealList/MealList';
 import { MEALS, CATEGORIES } from '../data/dummy-data';
-import MealItem from '../components/MealItem';
+
 import { useLayoutEffect } from 'react';
 
 function MealsOverviewScreen({ route, navigation }) {
@@ -16,39 +16,7 @@ function MealsOverviewScreen({ route, navigation }) {
     navigation.setOptions({ title: categoryTitle });
   }, [categoryId, navigation]);
 
-  function mealItemPressHandler(id) {
-    navigation.navigate('MealDetailScreen', { id });
-  }
-
-  function renderMealItem(itemData) {
-    const item = itemData.item;
-    const mealItemProps = {
-      id: item.id,
-      title: item.title,
-      imageUrl: item.imageUrl,
-      duration: item.duration,
-      complexity: item.complexity,
-      affordability: item.affordability,
-      onMealItemPress: mealItemPressHandler,
-    };
-    return <MealItem {...mealItemProps} />;
-  }
-  return (
-    <View styles={styles.container}>
-      <FlatList
-        data={displayedMeals}
-        keyExtractor={(item) => item.id}
-        renderItem={renderMealItem}
-      />
-    </View>
-  );
+  return <MealList items={displayedMeals} />;
 }
 
 export default MealsOverviewScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-  },
-});
