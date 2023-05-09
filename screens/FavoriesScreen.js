5b27d8502447577eb13f2044e7ada4afb64def5c
Ömer Okumuş <omerokumus3@gmail.com>
2023-05-09 13:12:15 +0300

    FavoritesScreen is implemented

--- screens/FavoriesScreen.js
+++ screens/FavoriesScreen.js
@@ -1,7 +1,15 @@
 import { Text } from 'react-native';
+import { MEALS } from '../data/dummy-data';
+import MealList from '../components/MealList/MealList';
+import { useContext } from 'react';
+import { FavoritesContext } from '../store/context/favorites-context';
 
 function FavoriesScreen() {
-  return <Text>FavoriesScreen</Text>;
+  const favoriteMealsCtx = useContext(FavoritesContext);
+  const favoriteMeals = MEALS.filter((meal) =>
+    favoriteMealsCtx.ids.includes(meal.id)
+  );
+  return <MealList items={favoriteMeals} />;
 }
 
 export default FavoriesScreen;
